ac3b64ab77b0646e53e004b14802149d8695250a
Andres Guevara <andy_jose_andres@hotmail.com>
2022-07-18 15:35:25 -0500

    Se configura react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import {Routes,Route} from "react-router-dom"
+import Home from './pages/Home/Home';
 function App() {
   return (
     <div className="App">
-      <p>Hola!!</p>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
